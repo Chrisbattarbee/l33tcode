b0926f9f30999d4e1aadd6e529a046c912221097
Chris Battarbee <cbattarbee@palantir.com>
2021-09-16 19:02:08 +0100

    Add palindrome

---
+++ src/leetcode/editor/en/[9]Palindrome Number.java
@@ -0,0 +1,66 @@
+package leetcode.editor.en;
+//Given an integer x, return true if x is palindrome integer.
+//
+// An integer is a palindrome when it reads the same backward as forward. For
+//example, 121 is palindrome while 123 is not.
+//
+//
+// Example 1:
+//
+//
+//Input: x = 121
+//Output: true
+//
+//
+// Example 2:
+//
+//
+//Input: x = -121
+//Output: false
+//Explanation: From left to right, it reads -121. From right to left, it
+//becomes 121-. Therefore it is not a palindrome.
+//
+//
+// Example 3:
+//
+//
+//Input: x = 10
+//Output: false
+//Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+//
+//
+// Example 4:
+//
+//
+//Input: x = -101
+//Output: false
+//
+//
+//
+// Constraints:
+//
+//
+// -2³¹ <= x <= 2³¹ - 1
+//
+//
+//
+//Follow up: Could you solve it without converting the integer to a string?
+//Related Topics Math 👍 3983 👎 1857
+
+
+import java.util.stream.IntStream;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        return IntStream.range(0, s.length() / 2)
+                .filter(i -> s.charAt(i) != s.charAt(s.length() - 1 - i))
+                .findFirst()
+                .isEmpty();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
